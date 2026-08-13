d47d94195af6c602bc5f77182747e9f625c59ac8
joelmaaparecidakorsak-del <joelmaaparecidakorsak@gmail.com>
2026-08-13 17:45:33 -0300

    Adiciona dataset e atualiza main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,3 +9,6 @@ print("pandas funcionando!")
 print("numpy funcionando!")
 print("matplotlib funcionando!")
 print("seaborn funcionando!")
+
+os.replace("nome,f"dataset/{base_varejo}
+           )
